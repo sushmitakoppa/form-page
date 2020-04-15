0f46e5b2f11963513860fced4c72d9c74550ad36
Sushmita Koppa <47295730+sushmitakoppa@users.noreply.github.com>
2020-04-15 13:04:40 +0530

    Update tag.js

--- tag.js
+++ tag.js
@@ -1,65 +1,91 @@
-let input,mainDiv,idNum=0;
-let tag=[],tags,html,count=0;
-let taglist=[];
 
 
-input=document.querySelector('#taginput');
-tags=input.value;
-mainDiv=document.querySelector("#master");
+const tagContainer = document.querySelector('.tag_container');
 
+const input= document.querySelector('.tag_container input');
 
-function add(){
-    tags=input.value;
-    tag=tags.split(/,|#|\s/);
-    tag = tag.filter(e => String(e).trim());
-    taglist=taglist.concat(tag);
-    console.log(taglist);
+let tags=[];
 
-    
-    tag.forEach(element => {
-        //creaing new span
-        let Span_new=document.createElement('span');
-        // Span_new.setAttribute('id',`span_`+(count+1));
-        Span_new.innerHTML=element;
-        //creating new button
-        let btn_new=document.createElement('button');
-        btn_new.setAttribute('id',`button_`+(count+1));
-        btn_new.setAttribute('class',`tag-btn`);
-        //onclick to delete that elements
-        btn_new.onclick=(event)=>{
-            //console.log(event);
-    
-            //tag to be reomnoved from array
-            delElemnt=Span_new.innerText;
-            console.log(taglist.splice(delElemnt, 1));
+function createTags(lable) {
 
-            //tag to be removed from display
-            delSpan=Span_new.parentElement.removeChild(Span_new);
-            console.log(delSpan);
-            console.log(tag);
-            console.log(taglist);
-            
+    //create div
+    const div=document.createElement("div");
+    div.setAttribute("class", "tag");
 
-        }
-        btn_new.innerHTML=`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">
-            <path fill="#000000" d="M15.854 12.854c-0-0-0-0-0-0l-4.854-4.854 4.854-4.854c0-0 0-0 0-0 0.052-0.052 0.090-0.113 0.114-0.178 0.066-0.178 0.028-0.386-0.114-0.529l-2.293-2.293c-0.143-0.143-0.351-0.181-0.529-0.114-0.065 0.024-0.126 0.062-0.178 0.114 0 0-0 0-0 0l-4.854 4.854-4.854-4.854c-0-0-0-0-0-0-0.052-0.052-0.113-0.090-0.178-0.114-0.178-0.066-0.386-0.029-0.529 0.114l-2.293 2.293c-0.143 0.143-0.181 0.351-0.114 0.529 0.024 0.065 0.062 0.126 0.114 0.178 0 0 0 0 0 0l4.854 4.854-4.854 4.854c-0 0-0 0-0 0-0.052 0.052-0.090 0.113-0.114 0.178-0.066 0.178-0.029 0.386 0.114 0.529l2.293 2.293c0.143 0.143 0.351 0.181 0.529 0.114 0.065-0.024 0.126-0.062 0.178-0.114 0-0 0-0 0-0l4.854-4.854 4.854 4.854c0 0 0 0 0 0 0.052 0.052 0.113 0.090 0.178 0.114 0.178 0.066 0.386 0.029 0.529-0.114l2.293-2.293c0.143-0.143 0.181-0.351 0.114-0.529-0.024-0.065-0.062-0.126-0.114-0.178z"></path></svg>`
-        count++;
-        Span_new.appendChild(btn_new);   
-        document.querySelector(`#master`).insertAdjacentElement('afterbegin',Span_new);
-        input.value="";  
-        
-    });   
+    //create span
+    const span=document.createElement("span");
+    span.innerHTML=lable;
+
+    //create i
+    const closebtn=document.createElement("i");
+    closebtn.setAttribute("class", "material-icons");
+    closebtn.setAttribute("id",lable);
+    closebtn.setAttribute("data-item",lable);
+    closebtn.innerHTML=`<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="16" height="16" viewBox="0 0 16 16">        <path fill="#000000" d="M15.854 12.854c-0-0-0-0-0-0l-4.854-4.854 4.854-4.854c0-0 0-0 0-0 0.052-0.052 0.090-0.113 0.114-0.178 0.066-0.178 0.028-0.386-0.114-0.529l-2.293-2.293c-0.143-0.143-0.351-0.181-0.529-0.114-0.065 0.024-0.126 0.062-0.178 0.114 0 0-0 0-0 0l-4.854 4.854-4.854-4.854c-0-0-0-0-0-0-0.052-0.052-0.113-0.090-0.178-0.114-0.178-0.066-0.386-0.029-0.529 0.114l-2.293 2.293c-0.143 0.143-0.181 0.351-0.114 0.529 0.024 0.065 0.062 0.126 0.114 0.178 0 0 0 0 0 0l4.854 4.854-4.854 4.854c-0 0-0 0-0 0-0.052 0.052-0.090 0.113-0.114 0.178-0.066 0.178-0.029 0.386 0.114 0.529l2.293 2.293c0.143 0.143 0.351 0.181 0.529 0.114 0.065-0.024 0.126-0.062 0.178-0.114 0-0 0-0 0-0l4.854-4.854 4.854 4.854c0 0 0 0 0 0 0.052 0.052 0.113 0.090 0.178 0.114 0.178 0.066 0.386 0.029 0.529-0.114l2.293-2.293c0.143-0.143 0.181-0.351 0.114-0.529-0.024-0.065-0.062-0.126-0.114-0.178z"></path></svg>`;
+    console.log(closebtn);
+
+    div.appendChild(span);
+    div.appendChild(closebtn);
+    return div;
 
 }
 
 
+//reset
+function reset() {
+    document.querySelectorAll('.tag').forEach(function(tag) {
+        tag.parentElement.removeChild(tag);
+        
+    })
+}
 
-input.addEventListener('keypress', function(event) {
-    if (event.keyCode === 13 || event.which === 13){
-        console.log("event:",tag);      
-        add();
+
+
+//add tags
+function addTags() {
+    reset();
+    tags.slice().reverse().forEach(function(tag) {
+        const input = createTags(tag);
+        tagContainer.prepend(input);
+        //console.log(tags);        
+    })
+    
+}
+
+//seprate each of input into tags
+input.addEventListener('keyup', (e) => {
+    if ((e.code === 'Space' || e.which === 32) || (e.key === 'Enter')){
+      e.target.value.split(/,|#/).forEach(tag => {
+        tags.push(tag);  
+      });
+      
+      addTags();
+      input.value = '';
     }
 });
 
 
-//module.exports =taglist
+
+
+
+//delete
+document.addEventListener('click', (e) => {
+  console.log(e.target.tagName);
+  console.log(e.target);
+  if(e.target.tagName === 'svg'){
+    e.target=console.log(e.target.parentElement);
+  }
+  if(e.target.tagName=== 'path'){
+    e.target=console.log(e.target.parentElement.parentElement);
+  }
+  
+  if(e.target.tagName === 'I') {
+    const tagLabel = e.target.getAttribute('data-item');
+    //console.log(tagLabel);
+    const index = tags.indexOf(tagLabel);
+    tags = [...tags.slice(0, index), ...tags.slice(index+1)];
+    addTags();    
+  }
+})
+
+
